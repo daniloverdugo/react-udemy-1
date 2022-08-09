adaacefb9b8a306a43cee3914bc2d566791c15f2
danilousach <danilo.verdugo@usach.cl>
2022-08-09 10:25:40 -0400

    modified:   src/CommentDetail.js 	modified:   src/index.js

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,25 +1,25 @@
-   
 import React from 'react';
-// existe librería por importar en sample???
+import faker from '@faker-js/faker';
 
-
-const NombreObjeto = () => {
-	return (
-<div className="ui container comments">
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img alt='avatar' src={faker.image.avatar()} />
+const CommentDetail = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
-        <div className='content'>
-          <a href='/' className='author'>
-            {faker.name.fullName({sex:"female"})}
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.fullName({ sex: 'female' })}
           </a>
-          <div className='metadata'>
-            <span className='date'>Hoy a las 10:00</span>
+          <div className="metadata">
+            <span className="date">Hoy a las 10:00</span>
           </div>
-          <div className='text'>{faker.lorem.sentence(6)}</div>
+          <div className="text">{faker.lorem.sentence(6)}</div>
         </div>
       </div>
     </div>
-	);
+  );
 };
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 // 1) librarias
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { faker } from "@faker-js/faker";
-
+import { faker } from '@faker-js/faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img alt='avatar' src={faker.image.avatar()} />
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
-        <div className='content'>
-          <a href='/' className='author'>
-            {faker.name.fullName({sex:"female"})}
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.fullName({ sex: 'female' })}
           </a>
-          <div className='metadata'>
-            <span className='date'>Hoy a las 10:00</span>
+          <div className="metadata">
+            <span className="date">Hoy a las 10:00</span>
           </div>
-          <div className='text'>{faker.lorem.sentence(6)}</div>
+          <div className="text">{faker.lorem.sentence(6)}</div>
         </div>
       </div>
     </div>
